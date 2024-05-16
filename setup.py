6843d44216f47d8632bea8ac235ea52fa5072054
Aymen kherraz <69644188+RedSn0w01@users.noreply.github.com>
2024-05-16 23:17:06 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     description='A simple invoice calculator',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/invoice_calculator',
-    author='Your Name',
-    author_email='your.email@example.com',
+    url='https://github.com/RedSn0w01/invoice_calculator.git',
+    author='aymen kherraz',
+    author_email='ayman.kherraz73@gmail.com',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
